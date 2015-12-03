d5fdbfd6140960ca20dff8b9ccfb45e5d5a58ea3
Andrea Bergamasco <info@vjandrea.net>
2015-12-03 21:45:40 +0100

    Wireframed class and tests

---
+++ src/Progress.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace Ealore\Progress;
+
+use Carbon\Carbon;
+
+class Progress
+{
+    protected $html = '';
+
+    protected $now;
+    protected $start;
+    protected $end;
+    protected $threshold;
+
+    protected $threshold_interval = 'P1M';
+
+    public function __construct()
+    {
+        $this->now = Carbon::now();
+        $this->start = Carbon::now()->subMonth();
+        $this->end = Carbon::now()->addMonth();
+    }
+
+    public function setStart($date = null)
+    {
+        $this->start = Carbon::parse($date);
+    }
+
+    public function setEnd($date = null)
+    {
+        $this->end = Carbon::parse($date);
+        $this->setThreshold();
+    }
+
+    public function setThreshold($date = null)
+    {
+        if(is_null($date) && isset($this->end) && !isset($this->threshold)) {
+            $this->threshold = $this->end->copy()->sub($this->getThresholdInterval());
+            return;
+        }
+
+        $this->threshold = Carbon::parse($date);
+    }
+
+    public function setThresholdInterval($interval = null)
+    {
+        $this->threshold_interval = $interval;
+    }
+
+    public function getStart()
+    {
+        return $this->start;
+    }
+
+    public function getEnd()
+    {
+        return $this->end;
+    }
+
+    public function getNow()
+    {
+        return $this->now;
+    }
+
+    public function getThresholdInterval()
+    {
+        return new \DateInterval($this->threshold_interval);
+    }
+
+    public function isAlive()
+    {
+        return $this->now < $this->end;
+    }
+
+    public function isSafe()
+    {
+        return $this->now < $this->threshold && $this->now < $this->end;
+    }
+
+    public function isExpiring()
+    {
+        return $this->now > $this->threshold && $this->now < $this->end;
+    }
+
+    // returns true when $end is not set
+    public function isExpired()
+    {
+        return $this->now > $this->end;
+    }
+
+    public function getColor()
+    {
+        if($this->isExpired()) {
+            return 'progress-bar-danger';
+        }
+
+        if($this->isExpiring()) {
+            return 'progress-bar-warning';
+        }
+
+        if($this->isAlive()) {
+            return 'progress-bar-success';
+        }
+
+        return 'progress-bar-info';
+    }
+
+    public function getLife()
+    {
+        return 40;
+    }
+
+    /*
+    <div class="progress">
+        <div class="progress-bar progress-bar-success" style="width: 35%">
+            <span class="sr-only">35% Complete (success)</span>
+        </div>
+        <div class="progress-bar progress-bar-warning progress-bar-striped" style="width: 20%">
+            <span class="sr-only">20% Complete (warning)</span>
+        </div>
+        <div class="progress-bar progress-bar-danger" style="width: 10%">
+            <span class="sr-only">10% Complete (danger)</span>
+        </div>
+    </div>
+    */
+    public function render()
+    {
+        $this->html = '<div class="progress">'
+        . '<div class="progress-bar ' . $this->getColor() . '" role="progressbar" aria-valuenow="' . $this->getLife() . '" aria-valuemin="0" aria-valuemax="100" style="width: ' . $this->getLife() . '%">'
+        . '<span class="sr-only">' . $this->getLife() . '% Complete</span>'
+        . '</div>'
+        . '</div>';
+
+        return $this->html;
+    }
+}
---
+++ tests/ProgressTest.php
@@ -0,0 +1,92 @@
+<?php
+
+use Ealore\Progress\Progress;
+
+class ProgressTest extends PHPUnit_Framework_TestCase {
+
+    public function testDates()
+    {
+        $progress = new Progress;
+
+        $progress->setStart('2012-02-03');
+        $progress->setEnd('2020-02-20');
+
+        $this->assertInstanceOf('Carbon\Carbon', $progress->getNow());
+        $this->assertInstanceOf('Carbon\Carbon', $progress->getStart());
+        $this->assertInstanceOf('Carbon\Carbon', $progress->getEnd());
+
+        $this->assertEquals('2012-02-03', $progress->getStart()->format('Y-m-d'));
+        $this->assertEquals('2020-02-20', $progress->getEnd()->format('Y-m-d'));
+    }
+
+    public function testThreshold()
+    {
+        $progress = new Progress;
+
+        $progress->setThresholdInterval('P2W'); // threshold is two weeks ago
+
+        $progress->setEnd(Carbon\Carbon::now()->addWeek()); // expires next week
+        $this->assertTrue($progress->isExpiring()); // so it's expiring
+        $this->assertTrue($progress->isAlive()); // still alive
+        $this->assertFalse($progress->isExpired()); // and not expired yet
+
+        $progress->setEnd(Carbon\Carbon::now()->addMonth()); // expires next month
+        $this->assertFalse($progress->isExpiring());
+        $this->assertTrue($progress->isAlive());
+        $this->assertFalse($progress->isExpired());
+
+        $progress->setEnd(Carbon\Carbon::now()->subMonth());
+        $this->assertFalse($progress->isExpiring());
+        $this->assertFalse($progress->isAlive());
+        $this->assertTrue($progress->isExpired());
+    }
+
+    public function testIsAlive()
+    {
+        $progress = new Progress;
+
+        $progress->setEnd('2030-01-01');
+
+        $this->assertTrue($progress->isAlive());
+    }
+
+    public function testIsExpiring()
+    {
+        $progress = new Progress;
+
+        $progress->setEnd(Carbon\Carbon::now()->copy()->addWeeks(2));
+
+        $this->assertTrue($progress->isAlive());
+        $this->assertTrue($progress->isExpiring());
+        $this->assertFalse($progress->isExpired());
+
+        $this->assertEquals(
+            $progress->getEnd()->format('Y-m-d'),
+            Carbon\Carbon::now()->copy()->addWeeks(2)->format('Y-m-d')
+        );
+    }
+
+    public function testIsExpired()
+    {
+        $progress = new Progress;
+
+        $progress->setEnd('2012-01-10');
+
+        $this->assertTrue($progress->isExpired());
+    }
+
+    public function testInitialized()
+    {
+        $progress = new Progress;
+
+        $this->assertTrue($progress->isAlive());
+        $this->assertFalse($progress->isExpired());
+    }
+
+    public function testRender()
+    {
+        $progress = new Progress;
+
+        $this->assertNotNull($progress->render());
+    }
+}
